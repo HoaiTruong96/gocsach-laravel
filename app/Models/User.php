805e9a126bbd39c3960676f2fada4ddee18ca890
ThongNguyen-64132336 <thong.nd.64cntt@ntu.edu.vn>
2025-12-11 14:13:21 +0700

    Nguyen duc thong (#40)
    * fix: Chỉnh sửa Review sang Post
    
    * fix: Chỉnh sửa Review sang Post
    
    * feat: thêm mới dashboard review và user
    
    * feat: Thêm chức năng
    
    - Controller:
    + Lịch sử hoạt động
    + Ghi log Sách, Danh mục, Bài đăng, Người dùng
    + Thêm xuất excel
    - Model:
    + Lịch sử hoạt động Admin
    + Xóa mềm Sách, Danh mục, Người dùng
    - Migration:
    + Lịch sử hoạt động Admin
    + Xóa mềm Sách, Danh mục, Người dùng, Bài đăng
    - Thêm chức năng & đường dẫn của Admin Panel:
    + Thêm xuất Excel dữ liệu tổng hợp của tháng
    + Thêm Lịch sử hoạt động dành cho Admin
    + Thêm chi tiết lịch sử hoạt động
    + Thêm thùng rác - Trường hợp xóa lịch sử hoạt động
    + Thêm dọn dẹp lịch sử hoạt động
    + Thêm khôi phục lịch sử hoạt động
    + Thêm xóa vĩnh viễn
    
    * chore: ẩn xóa mềm
    
    * feat: thêm migration comment like
    
    * feat: thêm xóa mềm
    
    * Update Post.php
    
    * chore: chỉnh để cập nhật lại model soft delete

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-#use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable; #, SoftDeletes;
+    use HasFactory, Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
