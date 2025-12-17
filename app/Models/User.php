4d0c07c78be12955a5d900b7b56745833ae2a999
HoaiTruong96 <hoai.tdt.64cntt@ntu.edu.vn>
2025-12-17 15:45:58 +0700

    Tronghoai (#70)
    * thay đổi giao diện home
    
    * sửa lỗi home và bookcontroller
    
    * thay giao diện detail
    
    * sửa lại giao diện home, và sắp xếp comment
    
    * sửa banner và phần tạp chí trên trang home
    
    * thêm cột deleted_at
    
    * sửa phần home
    
    * sửa articles
    
    * sửa hiển thị
    
    * sửa
    
    * sửa lại lỗi
    
    * sửa lỗi
    
    * sửa lỗi
    
    * sửa lỗi user

--- app/Models/User.php
+++ app/Models/User.php
@@ -197,10 +197,4 @@ class User extends Authenticatable
             }
         }
     }
-    public function isAdmin()
-    {
-        // Kiểm tra xem user có phải admin không
-        // Ví dụ: nếu cột role là 'admin' hoặc cột type là 1
-        return $this->role === 'admin';
-    }
 }
